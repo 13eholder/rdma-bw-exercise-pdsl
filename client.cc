3d5682e4ec8ea0dc2779160669ef7f5f19f1d439
13eholder <925349624@qq.com>
2024-09-13 12:09:48 +0800

    style:modify clang-format

--- client.cc
+++ client.cc
@@ -15,52 +15,64 @@
 
 int main()
 {
-    auto config = InitConifg(RdmaType::kClient);
-    RdmaContext client_ctx(config);
+	auto config = InitConifg(RdmaType::kClient);
+	RdmaContext client_ctx(config);
 
-    QPInfo remote_qp_info;
-    QPInfo local_qp_info = {.lid = client_ctx.port_attr_.lid, .qp_num = client_ctx.qp_->qp_num};
+	QPInfo remote_qp_info;
+	QPInfo local_qp_info = { .lid = client_ctx.port_attr_.lid,
+				 .qp_num = client_ctx.qp_->qp_num };
 
-    fmt::println("TcpClient connect to ip {}, port {}", config.ip, config.port);
+	fmt::println("TcpClient connect to ip {}, port {}", config.ip,
+		     config.port);
 
-    jsonrpc::TcpSocketClient tcp_client(config.ip, config.port);
-    jsonrpc::Client client(tcp_client);
+	jsonrpc::TcpSocketClient tcp_client(config.ip, config.port);
+	jsonrpc::Client client(tcp_client);
 
-    try {
-        auto value = local_qp_info.toJson();
-        auto resp = client.CallMethod("ExchangeQP", value);
-        remote_qp_info = QPInfo::parseJson(resp);
-        // modify qp to rts
-        RdmaModifyQp2Rts(client_ctx.qp_, remote_qp_info.qp_num, remote_qp_info.lid);
-        // post  a list of work requests to a send queue
-        for (int i = 0; i < kMsgNum; i++) {
-            ibv_send_wr* bad_send_wr;
-            ibv_sge list = {.addr = reinterpret_cast<uint64_t>(client_ctx.buf_ + i * kMsgSize),
-                            .length = kMsgSize,
-                            .lkey = client_ctx.mr_->lkey};
-            ibv_send_wr send_wr = {.wr_id = static_cast<uint64_t>(i),
-                                   .sg_list = &list,
-                                   .num_sge = 1,
-                                   .opcode = IBV_WR_SEND,
-                                   .send_flags = IBV_SEND_SIGNALED};
-            ErrCheck(ibv_post_send(client_ctx.qp_, &send_wr, &bad_send_wr) != 0, "Failed to post send");
-        }
-        //
-        uint32_t send_num = kMsgNum;
-        ibv_wc wc[kMaxPollSize];
-        while (send_num > 0) {
-            int n = ibv_poll_cq(client_ctx.cq_, kMaxPollSize, wc);
-            send_num -= n;
-            for (int i = 0; i < n; i++) {
-                if (wc[i].status != IBV_WC_SUCCESS) {
-                    fmt::println(stderr, "send failed status: {},wr_id={}", ibv_wc_status_str(wc[i].status),
-                                 wc[i].wr_id);
-                }
-            }
-        }
-    } catch (jsonrpc::JsonRpcException& e) {
-        ErrCheck(true, e.what());
-    }
-    fmt::println("finished");
-    return 0;
+	try {
+		auto value = local_qp_info.toJson();
+		auto resp = client.CallMethod("ExchangeQP", value);
+		remote_qp_info = QPInfo::parseJson(resp);
+		// modify qp to rts
+		RdmaModifyQp2Rts(client_ctx.qp_, remote_qp_info.qp_num,
+				 remote_qp_info.lid);
+		// post  a list of work requests to a send queue
+		for (int i = 0; i < kMsgNum; i++) {
+			ibv_send_wr *bad_send_wr;
+			ibv_sge list = { .addr = reinterpret_cast<uint64_t>(
+						 client_ctx.buf_ +
+						 i * kMsgSize),
+					 .length = kMsgSize,
+					 .lkey = client_ctx.mr_->lkey };
+			ibv_send_wr send_wr = {
+				.wr_id = static_cast<uint64_t>(i),
+				.sg_list = &list,
+				.num_sge = 1,
+				.opcode = IBV_WR_SEND,
+				.send_flags = IBV_SEND_SIGNALED
+			};
+			ErrCheck(ibv_post_send(client_ctx.qp_, &send_wr,
+					       &bad_send_wr) != 0,
+				 "Failed to post send");
+		}
+		//
+		uint32_t send_num = kMsgNum;
+		ibv_wc wc[kMaxPollSize];
+		while (send_num > 0) {
+			int n = ibv_poll_cq(client_ctx.cq_, kMaxPollSize, wc);
+			send_num -= n;
+			for (int i = 0; i < n; i++) {
+				if (wc[i].status != IBV_WC_SUCCESS) {
+					fmt::println(
+						stderr,
+						"send failed status: {},wr_id={}",
+						ibv_wc_status_str(wc[i].status),
+						wc[i].wr_id);
+				}
+			}
+		}
+	} catch (jsonrpc::JsonRpcException &e) {
+		ErrCheck(true, e.what());
+	}
+	fmt::println("finished");
+	return 0;
 }
--- rdma.h
+++ rdma.h
@@ -5,7 +5,7 @@
 #include <string>
 // 常量
 static const std::string kConfigPath = "config.json";
-static constexpr uint32_t kMsgSize = 4 * 1024 * 1024;  // 4MB
+static constexpr uint32_t kMsgSize = 4 * 1024 * 1024; // 4MB
 static constexpr uint32_t kMsgNum = 64;
 static constexpr uint32_t kBufSize = kMsgSize * kMsgNum;
 static constexpr int kQpNum = 1;
@@ -14,51 +14,51 @@ static constexpr uint8_t kIBSL = 0;
 static constexpr uint32_t kMaxPollSize = 16;
 // 结构体
 struct RdmaConfig {
-    uint16_t port{0};
-    std::string device;
-    std::string ip;
+	uint16_t port{ 0 };
+	std::string device;
+	std::string ip;
 };
 
 enum class RdmaType { kServer, kClient };
 
 struct QPInfo {
-    uint16_t lid;
-    uint32_t qp_num;
-    [[nodiscard]] Json::Value toJson() const
-    {
-        Json::Value value;
-        value["lid"] = lid;
-        value["qp_num"] = qp_num;
-        return value;
-    }
+	uint16_t lid;
+	uint32_t qp_num;
+	[[nodiscard]] Json::Value toJson() const
+	{
+		Json::Value value;
+		value["lid"] = lid;
+		value["qp_num"] = qp_num;
+		return value;
+	}
 
-    static QPInfo parseJson(const Json::Value& v)
-    {
-        return {.lid = static_cast<uint16_t>(v["lid"].asInt()), .qp_num = static_cast<uint32_t>(v["qp_num"].asInt())};
-    }
+	static QPInfo parseJson(const Json::Value &v)
+	{
+		return { .lid = static_cast<uint16_t>(v["lid"].asInt()),
+			 .qp_num = static_cast<uint32_t>(v["qp_num"].asInt()) };
+	}
 };
 
-class RdmaContext
-{
-public:
-    // func
-    explicit RdmaContext(const RdmaConfig& config);
-    ~RdmaContext();
-    // field
-    ibv_device** dev_list_{nullptr};
-    ibv_context* ctx_{nullptr};
-    ibv_pd* pd_{nullptr};
-    ibv_mr* mr_{nullptr};
-    ibv_cq* cq_{nullptr};
-    ibv_qp* qp_{nullptr};
-    char* buf_{nullptr};
-    ibv_device_attr dev_attr_;
-    ibv_port_attr port_attr_;
+class RdmaContext {
+    public:
+	// func
+	explicit RdmaContext(const RdmaConfig &config);
+	~RdmaContext();
+	// field
+	ibv_device **dev_list_{ nullptr };
+	ibv_context *ctx_{ nullptr };
+	ibv_pd *pd_{ nullptr };
+	ibv_mr *mr_{ nullptr };
+	ibv_cq *cq_{ nullptr };
+	ibv_qp *qp_{ nullptr };
+	char *buf_{ nullptr };
+	ibv_device_attr dev_attr_;
+	ibv_port_attr port_attr_;
 };
 
 // 宏
 
 // 函数
-void ErrCheck(bool cond, const char*);
+void ErrCheck(bool cond, const char *);
 RdmaConfig InitConifg(RdmaType type);
-void RdmaModifyQp2Rts(ibv_qp* qp, uint32_t target_qp_num, uint16_t target_lid);
+void RdmaModifyQp2Rts(ibv_qp *qp, uint32_t target_qp_num, uint16_t target_lid);
